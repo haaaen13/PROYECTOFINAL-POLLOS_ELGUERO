fa3f34b63aafbec2a696afa32cf74233633fbff1
l23330579-Rafael <l23330579@hermosillo.tecnm.mx>
2026-05-20 09:19:04 -0700

    Creacion del modelo usuario

---
+++ Models/Users.cs
@@ -0,0 +1,16 @@
+using backend.Models.Enums;
+
+namespace backend.Models;
+
+public class User
+{
+    public int Id { get; set; }
+
+    public string Username { get; set; } = string.Empty;
+
+    public string HashedPassword { get; set; } = string.Empty;
+
+    public UserRole Role { get; set; } = UserRole.Empleado;
+
+    
+}
