b889550a531e7cdad8f9272a01948cf6bae518a9
Haen <haensito@gmail.com>
2026-05-24 11:44:37 -0700

    Se agregó la carpeta sucursales con sus respectivos DTOS

---
+++ Dtos/Sucursales/CreateSucursalDto.cs
@@ -0,0 +1,8 @@
+namespace backend.Dtos.Sucursales;
+
+public class CreateSucursalDto
+{
+    public string Nombre { get; set; } = string.Empty;
+
+    public string Direccion { get; set; } = string.Empty;
+}
---
+++ Dtos/Sucursales/SucursalResponseDto.cs
@@ -0,0 +1,10 @@
+namespace backend.Dtos.Sucursales;
+
+public class SucursalResponseDto
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = string.Empty;
+
+    public string Direccion { get; set; } = string.Empty;
+}
