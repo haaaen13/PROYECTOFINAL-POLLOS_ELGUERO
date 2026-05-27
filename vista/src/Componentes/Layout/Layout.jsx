3a603820b949f69a92c95fd5587584a58382f50f
sebastian.barrios <sebastian.barrios.lop@gmail.com>
2026-05-26 17:31:45 -0700

    Componente para tener contenido en la derecha de la sidebar

---
+++ vista/src/Componentes/Layout/Layout.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom";
+
+import Sidebar from "../sidebar/sidebar";
+
+import "./Layout.css";
+
+function Layout() {
+  return (
+    <div className="layout">
+      <Sidebar />
+
+      <main className="main-content">
+        <Outlet />
+      </main>
+    </div>
+  );
+}
+
+export default Layout;
