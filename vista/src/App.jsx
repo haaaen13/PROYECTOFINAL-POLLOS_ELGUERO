070d4784181df0270e4195e6ad41a18073175cbe
sebastian.barrios <sebastian.barrios.lop@gmail.com>
2026-05-26 18:03:10 -0700

    Cambios en main y routero(app)

--- vista/src/App.jsx
+++ vista/src/App.jsx
@@ -1,122 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import "./App.css";
+
+import { Routes, Route, Navigate } from "react-router-dom";
+
+/*
+  COMPONENTES
+*/
+
+import Login from "./Componentes/Login";
+
+import Layout from "./Componentes/Layout/Layout";
+
+/*
+  ADMIN
+*/
+
+import Empleados from "./Componentes/Empleados";
+import Productos from "./Componentes/Productos/Productos";
+import Sucursales from "./Componentes/Sucursales/Sucursales";
+import Ventas from "./Componentes/Ventas/Ventas";
+import TurnosActivos from "./Componentes/TurnosActivos/TurnosActivos";
+
+import PosContainer from "./Componentes/POS/PosContainer";
+import MisVentas from "./Componentes/MisVentas/MisVentas";
+import MisMovimientos from "./Componentes/MisMovimientos/MisMovimientos";
+import CierreCaja from "./Componentes/Cierre/CierreCaja";
+import Dashboard from "./Componentes/Dashboard/Dashboard";
+
+import {
+  PrivateAdminRoute,
+  PrivateEmpleadoRoute,
+} from "./Componentes/PrivateRoutes";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <Routes>
+      <Route path="/" element={<Login />} />
 
-      <div className="ticks"></div>
+      <Route path="*" element={<Navigate to="/" />} />
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+      <Route element={<PrivateAdminRoute />}>
+        <Route element={<Layout />}>
+          <Route path="/ventas" element={<Ventas />} />
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+          <Route path="/turnos" element={<TurnosActivos />} />
+
+          <Route path="/empleados" element={<Empleados />} />
+
+          <Route path="/productos" element={<Productos />} />
+
+          <Route path="/sucursales" element={<Sucursales />} />
+
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+      </Route>
+
+      {/* RUTAS EMPLEADO */}
+
+      <Route element={<PrivateEmpleadoRoute />}>
+        <Route element={<Layout />}>
+          <Route path="/pos" element={<PosContainer />} />
+
+          <Route path="/mis-ventas" element={<MisVentas />} />
+
+          <Route path="/caja" element={<MisMovimientos />} />
+
+          <Route path="/cierre" element={<CierreCaja />} />
+        </Route>
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- vista/src/main.jsx
+++ vista/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store, persistor } from "./Utilidades/Redux/store.jsx";
+import { PersistGate } from "redux-persist/integration/react";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </BrowserRouter>,
+);
