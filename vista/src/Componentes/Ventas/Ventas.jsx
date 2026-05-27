25d69ebf433269f75fa8f9c776549ce1f3e58d7c
sebastian.barrios <sebastian.barrios.lop@gmail.com>
2026-05-26 17:39:43 -0700

    Mostrar ventas realizadas al admin

---
+++ vista/src/Componentes/Ventas/Ventas.jsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import TableToolbar from "../TableToolbar/TableToolbar";
+import "../TableToolbar/TableToolbar.css";
+import Pagination from "../Pagination/Pagination";
+import usePagination from "../../hooks/usePagination";
+
+import { listarVentas } from "../../Utilidades/Redux/Ventas/VentasAction";
+import TablaVentas from "../TablaVentas/TablaVentas";
+
+function Ventas() {
+  const dispatch = useDispatch();
+
+  const ventas = useSelector((store) => store.ventas.Ventas);
+
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    dispatch(listarVentas());
+  }, [dispatch]);
+
+  const {
+    currentPage,
+    totalPages,
+    paginatedData,
+    nextPage,
+    prevPage,
+    goToPage,
+    resetPage,
+  } = usePagination(ventas, 10);
+
+  return (
+    <>
+      <div className="tabla-container">
+        <TableToolbar
+          title="Ventas"
+          searchPlaceholder="Buscar Venta..."
+          buttonText="xd"
+          onSearch={(value) => {
+            setSearch(value);
+
+            resetPage();
+          }}
+          onAdd={() => setOpenModal(true)}
+          showButton={false}
+        />
+
+        <TablaVentas ventas={paginatedData} />
+      </div>
+
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={goToPage}
+        onNext={nextPage}
+        onPrev={prevPage}
+      />
+    </>
+  );
+}
+
+export default Ventas;
