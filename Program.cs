23983917944c6c3437794aca5b86a3579fcf1772
sebastian.barrios <sebastian.barrios.lop@gmail.com>
2026-05-26 18:32:50 -0700

    Cambios para conectarse a Sql Server

--- Program.cs
+++ Program.cs
@@ -15,7 +15,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    )
 );
 
 builder.Services.AddCors(options =>
